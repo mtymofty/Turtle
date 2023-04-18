9aa480d2dd1a17cb46e4229d3002f1cb0bf01c4c
Maciej Tymoftyjewicz <01158968@pw.edu.pl>
2023-04-18 18:30:15 +0200

    reader implementation

--- src/Reader.ts
+++ src/Reader.ts
@@ -30,27 +30,7 @@ export class FileReader implements Reader {
 		if (read == 0){
 			return null;
 		}
-		return buf.toString();
-	}
-
-	get_char2() {
-		let buf = Buffer.alloc(1, 0);
-		let fd: number | undefined;
-		let read: number;
-
-		try {
-			fd = fs.openSync(this.file_path, "r");
-			read = fs.readSync(fd, buf, 0, 1, this.file_position);
-		} finally {
-			if (fd) {
-				fs.closeSync(fd);
-			}
-		}
-		this.file_position += 1;
-		if (read == 0){
-			return null;
-		}
-		return buf;
+		return JSON.stringify(buf.toString()).slice(1, -1);
 	}
 }
 
@@ -69,7 +49,7 @@ export class StringReader implements Reader {
 		if (this.len != this.position) {
 			let char = this.data[this.position];
 			this.position += 1;
-			return char;
+			return JSON.stringify(char).slice(1, -1);
 		} else {
 			return null;
 		}
