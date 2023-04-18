63fc0f8147963c807dc922153af2db38b14dd693
Maciej Tymoftyjewicz <01158968@pw.edu.pl>
2023-04-18 18:35:22 +0200

    moved position from Reader class

--- src/Reader.ts
+++ src/Reader.ts
@@ -1,32 +1,29 @@
 import * as fs from 'fs';
 
 export interface Reader {
-	get_char(): string | null;
+	get_char(pos: number): string | null;
 }
 
 export class FileReader implements Reader {
-	file_position: number;
 	file_path: string;
 
 	constructor(path: string) {
 		this.file_path = path;
-		this.file_position = 0;
 	}
 
-	get_char() {
+	get_char(pos: number) {
 		let buf = Buffer.alloc(1, 0);
 		let fd: number | undefined;
 		let read: number;
 
 		try {
 			fd = fs.openSync(this.file_path, "r");
-			read = fs.readSync(fd, buf, 0, 1, this.file_position);
+			read = fs.readSync(fd, buf, 0, 1, pos);
 		} finally {
 			if (fd) {
 				fs.closeSync(fd);
 			}
 		}
-		this.file_position += 1;
 		if (read == 0){
 			return null;
 		}
@@ -35,20 +32,17 @@ export class FileReader implements Reader {
 }
 
 export class StringReader implements Reader {
-	position: number;
 	data: string;
 	len: number;
 
 	constructor(data: string) {
-		this.position = 0;
 		this.data = data;
 		this.len = data.length;
 	}
 
-	get_char() {
-		if (this.len != this.position) {
-			let char = this.data[this.position];
-			this.position += 1;
+	get_char(pos: number) {
+		if (this.len != pos) {
+			let char = this.data[pos];
 			return JSON.stringify(char).slice(1, -1);
 		} else {
 			return null;
